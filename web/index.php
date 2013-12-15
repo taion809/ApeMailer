3b2b74c1a464dc231315f56bc888e2151915a4ac
Nicholas J <nicholas.a.johns5@gmail.com>
2013-12-15 14:47:54 +0000

    Updated the initial structure of the application

--- web/index.php
+++ web/index.php
@@ -2,10 +2,36 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Johnsn\GuerrillaMail\GuerrillaConnect\CurlConnection;
+use Johnsn\GuerrillaMail\GuerrillaMail;
+
 $app = new Silex\Application();
 
-$app->get('/hello', function() {
-    return 'Hello!';
+$app['gm.connection'] = function() {
+    return new CurlConnection('127.0.0.1');
+};
+
+$app['gm.client'] = function($app) {
+    return new GuerrillaMail($app['gm.connection']);
+};
+
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('new', function() use ($app) {
+    $client = $app['gm.client'];
+
+    $response = $client->get_email_address();
+
+    var_dump($response);
+
+    return true;
+});
+
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('index.twig');
 });
 
 $app->run();
